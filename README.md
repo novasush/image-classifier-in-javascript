b35b6d8d82999b263c560aa3f5cc13a76f65321a
novasush <novasush@nsemble.ai>
2020-05-26 18:24:43 +0530

    updated code for blog

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# cat-dog-classifier-in-javascript-
-Cat Dog classifier using tensorflowjs
+# A cat dog image classifier deployed client side using javascript
+## Live demo link
+[Cat Dog Classifer in JavaScript](https://novasush.com/projects/classifier.html)
+
+![image](https://novasush.com/blog/images/cat_vs_dog_in_javascript.jpg)
+
+# Instructions
+1. Train a keras classifier and save its weights
+2. install `tensorflowjs` python library
+```bash
+pip install tensorflowjs
+```
+3. Use tensorflowjs converter tool to convert h5 weights to js format.
+```bash
+tensorflowjs_converter --input_format=keras ./model.h5 ./jsweights
+```
+* Make sure the .h5 weights location is correct and also destination path exists.
+
+### After running above command you will see `model.json` and `.bin` files, tensorflowjs converter dumps model architecture in `model.json` and it's weights in `.bin` files.
+
+# setup
+* Place your converted weights inside `weights` foler
+
+* Open `classifier.js` and update `model.json` path according to your weights folder
+
+* After everything is setup up perfectly run below command inside the directory.
+
+```bash
+python3 -m http.server
+```
+
+* navigate to [localhost](http:127.0.0.1) 
+
+# Output
+
+![courage_garfield](https://novasush.com/blog/images/courage_garfield.jpg)
---
+++ static/classifier.js
@@ -0,0 +1,137 @@
+//========================================================================
+// Drag and drop image handling
+//========================================================================
+
+var fileDrag = document.getElementById("file-drag");
+var fileSelect = document.getElementById("file-upload");
+
+// Add event listeners
+fileDrag.addEventListener("dragover", fileDragHover, false);
+fileDrag.addEventListener("dragleave", fileDragHover, false);
+fileDrag.addEventListener("drop", fileSelectHandler, false);
+fileSelect.addEventListener("change", fileSelectHandler, false);
+
+function fileDragHover(e) {
+  // prevent default behaviour
+  e.preventDefault();
+  e.stopPropagation();
+
+  fileDrag.className = e.type === "dragover" ? "upload-box dragover" : "upload-box";
+}
+
+function fileSelectHandler(e) {
+  // handle file selecting
+  var files = e.target.files || e.dataTransfer.files;
+  fileDragHover(e);
+  for (var i = 0, f; (f = files[i]); i++) {
+    previewFile(f);
+  }
+}
+
+//========================================================================
+// Web page elements for functions to use
+//========================================================================
+
+var imagePreview = document.getElementById("image-preview");
+var imageDisplay = document.getElementById("image-display");
+var uploadCaption = document.getElementById("upload-caption");
+var predResult = document.getElementById("pred-result2");
+var loader = document.getElementById("loader");
+var model = undefined;
+
+//========================================================================
+// Main button events
+//========================================================================
+
+
+async function initialize() {
+
+    model = await tf.loadLayersModel('/weights/catsvsdogs/model.json');
+
+    // document.getElementById('predict').addEventListener('click', () => predict());
+
+}
+
+async function predict() {
+  // action for the submit button
+  if (!imageDisplay.src || !imageDisplay.src.startsWith("data")) {
+    window.alert("Please select an image before submit.");
+    return;
+  }
+
+  let tensorImg = tf.browser.fromPixels(imagePreview).resizeNearestNeighbor([150, 150]).toFloat().expandDims();
+  prediction = await model.predict(tensorImg).data();
+
+  if (prediction[0] === 0) {
+      predResult.innerHTML = "I think it's a cat";
+
+  } else if (prediction[0] === 1) {
+      predResult.innerHTML = "I think it's a dog";
+
+  } else {
+      predResult.innerHTML = "This is Something else";
+  }
+  show(predResult)
+
+}
+
+function clearImage() {
+  // reset selected files
+  fileSelect.value = "";
+
+  // remove image sources and hide them
+  imagePreview.src = "";
+  imageDisplay.src = "";
+  predResult.innerHTML = "";
+
+  hide(imagePreview);
+  hide(imageDisplay);
+  hide(loader);
+  hide(predResult);
+  show(uploadCaption);
+
+  imageDisplay.classList.remove("loading");
+}
+
+function previewFile(file) {
+  // show the preview of the image
+  var fileName = encodeURI(file.name);
+
+  var reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = () => {
+    imagePreview.src = URL.createObjectURL(file);
+
+    show(imagePreview);
+    hide(uploadCaption);
+
+    // reset
+    predResult.innerHTML = "";
+    imageDisplay.classList.remove("loading");
+
+    displayImage(reader.result, "image-display");
+  };
+}
+
+//========================================================================
+// Helper functions
+//========================================================================
+
+function displayImage(image, id) {
+  // display image on given id <img> element
+  let display = document.getElementById(id);
+  display.src = image;
+  show(display);
+}
+
+function hide(el) {
+  // hide an element
+  el.classList.add("hidden");
+}
+
+function show(el) {
+  // show an element
+  el.classList.remove("hidden");
+}
+
+initialize();
